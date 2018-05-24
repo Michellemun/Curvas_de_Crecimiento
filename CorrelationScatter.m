9e67bc0b3d2d2d27f2f676e6f4e60c868bffda1f
AbrahamAvelar <jabrahamavelar@gmail.com>
2018-05-24 11:34:31 -0500

    Update CorrelationScatter.m

--- CorrelationScatter.m
+++ CorrelationScatter.m
@@ -1,5 +1,6 @@
 function [plotHandle r p] = CorrelationScatter(x,y,labelx, labely, namesx, namesy, textos)
 % [plotHandle r p] = CorrelationScatter(x,y,labely,namesx,namesx,namesy)
+% los primeros 4 parametros son obligatorios
 error(nargchk(0,7,nargin))
 if length(x) ~= length(y)
     warning('X and Y have different sizes');
@@ -19,7 +20,7 @@ r=r(2);
 p=p(2);
 
 plotHandle = plot(x(validas),y(validas),'o');
-legend(strcat(' r= ',num2str(r),' p= ',num2str(p)),'location','best')
+title(strcat(' r= ',num2str(r),' p= ',num2str(p), ' n=', num2str(length(validas)) ),'location','best')
 
 fastLabels(labelx, labely, 'Scatter')
 
